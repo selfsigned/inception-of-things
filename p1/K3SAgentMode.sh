d2d89f5f4fb60f48531cab72c97c008bd1f36737
Pierre CHAMBON-DUCASSE <pchambon@e1r3p6.clusters.42paris.fr>
2022-12-06 15:14:22 +0100

    test

--- p1/K3SAgentMode.sh
+++ p1/K3SAgentMode.sh
@@ -1,4 +1,5 @@
 echo "[$(hostname)] Installing K3S on agent."
+export K3S_TOKEN=$(cat /sync/token)
 export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6643 --node-ip=192.168.56.111"
 curl -sfL https://get.k3s.io | sh -
 echo "[$(hostname)] Configured succesfully"

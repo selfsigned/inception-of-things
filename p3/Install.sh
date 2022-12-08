907f3e81dc0970a94dfd5468c3dcb2d3942eaa93
Pierre CHAMBON-DUCASSE <pchambon@e1r13p3.clusters.42paris.fr>
2022-12-08 12:27:52 +0100

    ingress

--- p3/Install.sh
+++ p3/Install.sh
@@ -1,12 +1,20 @@
+echo "[$(hostname)] Installing Docker"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo rc boot docker
 echo "[$(hostname)] Installing K3D on controller."
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8888:8888@loadbalancer --port 8443:443@loadbalancer
+echo "[$(hostname)] Installing Kubectl on controller."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.25.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sleep 15
 echo "[$(hostname)] Installing ArgoCD"
 kubectl create namespace argocd
 kubectl create namespace dev
 wget https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml -O install.yaml
 kubectl apply -f install.yaml -n argocd
-echo "[$(hostname)] Installing Docker"
-apk add --update docker openrc
 echo "[$(hostname)] Deploying Ingress"
-kubectl apply -f /sync/ingress/ingress.yaml
+kubectl apply -f /sync/yaml/ingress.yaml
 echo "[$(hostname)] Configured succesfully"
